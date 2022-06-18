6573e884d1e0532e3d4150724ad6c248f4aee888
dbrito1992 <1992diego1992@gmail.com>
2022-06-18 00:24:08 -0300

    webpack part2

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./src/webpack1/index.ts ***!
+  \*******************************/
+
+console.log('Ola Mundo!');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
